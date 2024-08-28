6424ddbf32a8e3f3901d960148ffa067e8913aa6
uwi0 <kakapoweb@gmail.com>
2024-08-29 02:58:18 +0700

    Initial Commit

---
+++ Tool Tip VIew/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tool Tip VIew
+//
+//  Created by dwi prasetyo on 29/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tool Tip VIew/Tool_Tip_VIewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tool_Tip_VIewApp.swift
+//  Tool Tip VIew
+//
+//  Created by dwi prasetyo on 29/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Tool_Tip_VIewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
